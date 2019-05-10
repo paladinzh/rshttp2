6a4ee9055af9eff83c8a721e7e39b1bbdbf75b15
Taoda <tyf00@aliyun.com>
2019-05-10 10:25:34 +0800

    new dir structure of mod hpack

---
+++ src/hpack/mod.rs
@@ -0,0 +1,310 @@
+mod static_table;
+use static_table::*;
+
+pub struct Context {
+    static_table_seeker: static_table::Seeker,
+}
+
+impl Context {
+    pub fn new() -> Context {
+        Context{
+            static_table_seeker: static_table::Seeker::new()}
+    }
+}
+
+
+fn parse_uint(
+    b: *const u8,
+    e: *const u8,
+    prefix_bits: usize,
+) -> Result<(*const u8, u64), &'static str> {
+    if b >= e {
+        return Err("shortage of buf on deserialization.");
+    }
+
+    let mask = ((1 << prefix_bits) - 1) as u8;
+    let first_byte = unsafe {*b & mask};
+    let mut b = unsafe {b.add(1)};
+    if first_byte < mask {
+        return Ok((b, first_byte as u64))
+    }
+
+    let mut buf = vec!();
+    loop {
+        if b >= e {
+            return Err("shortage of buf on deserialization.");
+        }
+        let byte = unsafe {*b};
+        b = unsafe {b.add(1)};
+        buf.push(byte & 0x7Fu8);
+        if byte & 0x80u8 == 0 {
+            break;
+        }
+    }
+    let mut res = 0u64;
+    loop {
+        match buf.pop() {
+            Some(b) => {
+                res <<= 7;
+                res |= b as u64;
+            },
+            None => {
+                break;
+            }
+        }
+    }
+    res += mask as u64;
+    
+    Ok((b, res))
+}
+
+fn serialize_uint(
+    out: &mut Vec<u8>,
+    v: u64,
+    prefix_bits: usize,
+    first_byte_flags: u8,
+) -> Result<(), &'static str> {
+    let prefix_mask = ((1 << prefix_bits) - 1) as u8;
+    let flag_mask = !prefix_mask;
+    let mut v = v;
+
+    if v < prefix_mask as u64 {
+        let mut res = (v & prefix_mask as u64) as u8;
+        res |= first_byte_flags & flag_mask;
+        out.push(res);
+        return Ok(())
+    }
+
+    {
+        let res = (first_byte_flags & flag_mask) | prefix_mask;
+        out.push(res);
+        v -= prefix_mask as u64;
+    }
+
+    while v > 0x7F {
+        let res = 0x80 | ((v & 0x7F) as u8);
+        out.push(res);
+        v >>= 7;
+    }
+
+    {
+        let res = (v & 0x7F) as u8;
+        out.push(res);
+    }
+
+    Ok(())
+}
+
+fn serialize_raw_string(
+    out: &mut Vec<u8>,
+    value: &[u8],
+) -> Result<(), &'static str> {
+    serialize_uint(out, value.len() as u64, 7, 0);
+    out.extend_from_slice(value);
+    Ok(())
+}
+
+fn parse_string(
+    b: *const u8,
+    e: *const u8,
+) -> Result<(*const u8, Vec<u8>), &'static str> {
+    if b >= e {
+        return Err("shortage of buf on deserialization.");
+    }
+
+    let byte = unsafe {*b};
+    if (byte & 0x80) == 0 {
+        let (b, len) = parse_uint(b, e, 7)?;
+        let len = len as usize;
+        let mut b = b;
+        if unsafe {b.add(len)} > e {
+            return Err("shortage of buf on deserialization.");
+        }
+        let mut res = Vec::<u8>::with_capacity(len);
+        while b < e {
+            unsafe {
+                res.push(*b);
+                b = b.add(1);
+            }
+        }
+        Ok((b, res))
+    } else {
+        let (b, len) = parse_uint(b, e, 7)?;
+        let len = len as usize;
+        let e1 = unsafe {b.add(len)};
+        if e1 > e {
+            return Err("shortage of buf on deserialization.");
+        }
+        decode_huffman(b, e1)
+    }
+}
+
+fn decode_huffman(
+    b: *const u8,
+    e: *const u8,
+) -> Result<(*const u8, Vec<u8>), &'static str> {
+    Ok((b, vec!()))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_uint_0() {
+        let buf = vec!(0u8);
+        let b = buf.as_ptr();
+        let e = unsafe {b.add(buf.len())};
+        let (b, res) = parse_uint(b, e, 5).unwrap();
+        assert_eq!(b, e);
+        assert_eq!(res, 0);
+    }
+
+    #[test]
+    fn test_parse_uint_1() {
+        let buf = vec!(0x0Au8);
+        let b = buf.as_ptr();
+        let e = unsafe {b.add(buf.len())};
+        let (b, res) = parse_uint(b, e, 5).unwrap();
+        assert_eq!(b, e);
+        assert_eq!(res, 10);
+    }
+
+    #[test]
+    fn test_parse_uint_2() {
+        let buf = vec!(31u8, 154u8, 10u8);
+        let b = buf.as_ptr();
+        let e = unsafe {b.add(buf.len())};
+        let (b, res) = parse_uint(b, e, 5).unwrap();
+        assert_eq!(b, e);
+        assert_eq!(res, 1337);
+    }
+
+    #[test]
+    fn test_parse_uint_err0() {
+        let buf: Vec<u8> = vec!();
+        let b = buf.as_ptr();
+        let e = unsafe {b.add(buf.len())};
+        let err = parse_uint(b, e, 5);
+        assert!(err.is_err());
+    }
+    
+    #[test]
+    fn test_parse_uint_err1() {
+        let buf: Vec<u8> = vec!(31u8, 154u8);
+        let b = buf.as_ptr();
+        let e = unsafe {b.add(buf.len())};
+        let err = parse_uint(b, e, 5);
+        assert!(err.is_err());
+    }
+
+    #[test]
+    fn test_serialize_uint_0() {
+        let mut buf: Vec<u8> = vec!();
+        let _ = serialize_uint(&mut buf, 0, 5, 0).unwrap();
+        assert_eq!(buf, [0]);
+    }
+
+    #[test]
+    fn test_serialize_uint_1() {
+        let mut buf: Vec<u8> = vec!();
+        let _ = serialize_uint(&mut buf, 10, 5, 0xA0).unwrap();
+        assert_eq!(buf, [0xAA]);
+    }
+
+    #[test]
+    fn test_serialize_uint_2() {
+        let mut buf: Vec<u8> = vec!();
+        let _ = serialize_uint(&mut buf, 1337, 5, 0).unwrap();
+        assert_eq!(buf, [31u8, 154u8, 10u8]);
+    }
+
+    #[test]
+    fn test_serialize_uint_3() {
+        let mut buf: Vec<u8> = vec!();
+        let _ = serialize_uint(&mut buf, 31, 5, 0).unwrap();
+        assert_eq!(buf, [31, 0]);
+    }
+
+    #[test]
+    fn test_serialize_parse_uint_exhaustive() {
+        for prefix_bits in 1usize..9usize {
+            for oracle_value in 0u64..2000u64 {
+                let mut buf = vec!();
+                let _ = serialize_uint(&mut buf, oracle_value, prefix_bits, 0)
+                    .unwrap();
+
+                let b = buf.as_ptr();
+                let e = unsafe {b.add(buf.len())};
+                let (b, trial_value) = parse_uint(b, e, prefix_bits)
+                    .unwrap();
+
+                assert_eq!(trial_value, oracle_value);
+                assert_eq!(b, e);
+            }
+        }
+    }
+
+    #[test]
+    fn test_serialize_raw_string_0() {
+        let mut buf: Vec<u8> = vec!();
+        let res = serialize_raw_string(&mut buf, b"");
+        assert!(res.is_ok());
+        assert_eq!(buf, [0]);
+    }
+
+    #[test]
+    fn test_serialize_raw_string_1() {
+        let mut buf: Vec<u8> = vec!();
+        let res = serialize_raw_string(&mut buf, b"custom-key");
+        assert!(res.is_ok());
+        assert_eq!(
+            buf,
+            [0x0A, 0x63, 0x75, 0x73, 0x74, 0x6F, 0x6D, 0x2D, 0x6B, 0x65, 0x79]);
+    }
+
+    #[test]
+    fn test_parse_raw_string_0() {
+        let buf = vec!(0u8);
+        let b = buf.as_ptr();
+        let e = unsafe {b.add(buf.len())};
+        let (b, res) = parse_string(b, e).unwrap();
+        assert_eq!(b, e);
+        assert_eq!(res, []);
+    }
+
+    #[test]
+    fn test_parse_raw_string_1() {
+        let buf = vec![0x0A, 0x63, 0x75, 0x73, 0x74, 0x6F, 0x6D, 0x2D, 0x6B, 0x65, 0x79];
+        let b = buf.as_ptr();
+        let e = unsafe {b.add(buf.len())};
+        let (b, res) = parse_string(b, e).unwrap();
+        assert_eq!(b, e);
+        assert_eq!(res, b"custom-key");
+    }
+
+    #[test]
+    fn test_parse_huffman_string_0() {
+        let buf = vec!(0x80u8);
+        let b = buf.as_ptr();
+        let e = unsafe {b.add(buf.len())};
+        let (b, res) = parse_string(b, e).unwrap();
+        assert_eq!(b, e);
+        assert_eq!(res, []);
+    }
+
+
+    #[test]
+    fn test_parse_huffman_string_1() {
+        let buf = vec![
+            0x8C, 0xF1, 0xE3, 0xC2, 0xE5,
+            0xF1, 0x2A, 0x6B, 0xA0, 0xAB,
+            0x90, 0xF4, 0xFF];
+        let b = buf.as_ptr();
+        let e = unsafe {b.add(buf.len())};
+        let (b, res) = parse_string(b, e).unwrap();
+        assert_eq!(b, e);
+        assert_eq!(res, b"www.example.com");
+    }
+
+}
---
+++ src/hpack/static_table.rs
@@ -0,0 +1,186 @@
+use std::collections::BTreeMap;
+
+#[derive(Debug)]
+pub struct Item {
+    name: &'static [u8],
+    value: Option<&'static [u8]>,
+}
+
+pub const RAW_TABLE: [Item; 62] = [
+    Item{name: b"", value: None},
+
+    Item{name: b":authority", value: None},
+    Item{name: b":method", value: Some(b"GET")},
+    Item{name: b":method", value: Some(b"POST")},
+    Item{name: b":path", value: Some(b"/")},
+    Item{name: b":path", value: Some(b"/index.html")},
+
+    Item{name: b":scheme", value: Some(b"http")},
+    Item{name: b":scheme", value: Some(b"https")},
+    Item{name: b":status", value: Some(b"200")},
+    Item{name: b":status", value: Some(b"204")},
+    Item{name: b":status", value: Some(b"206")},
+    Item{name: b":status", value: Some(b"304")},
+    Item{name: b":status", value: Some(b"400")},
+    Item{name: b":status", value: Some(b"404")},
+    Item{name: b":status", value: Some(b"500")},
+
+    Item{name: b"accept-charset", value: None},
+    Item{name: b"accept-encoding", value: Some(b"gzip, deflate")},
+    Item{name: b"accept-language", value: None},
+    Item{name: b"accept-ranges", value: None},
+    Item{name: b"accept", value: None},
+    Item{name: b"access-control-allow-origin", value: None},
+    Item{name: b"age", value: None},
+    Item{name: b"allow", value: None},
+    Item{name: b"authorization", value: None},
+
+    Item{name: b"cache-control", value: None},
+    Item{name: b"content-disposition", value: None},
+    Item{name: b"content-encoding", value: None},
+    Item{name: b"content-language", value: None},
+    Item{name: b"content-length", value: None},
+    Item{name: b"content-location", value: None},
+    Item{name: b"content-range", value: None},
+    Item{name: b"content-type", value: None},
+    Item{name: b"cookie", value: None},
+
+    Item{name: b"date", value: None},
+    Item{name: b"etag", value: None},
+    Item{name: b"expect", value: None},
+    Item{name: b"expires", value: None},
+    Item{name: b"from", value: None},
+    Item{name: b"host", value: None},
+
+    Item{name: b"if-match", value: None},
+    Item{name: b"if-modified-since", value: None},
+    Item{name: b"if-none-match", value: None},
+    Item{name: b"if-range", value: None},
+    Item{name: b"if-unmodified-since", value: None},
+
+    Item{name: b"last-modified", value: None},
+    Item{name: b"link", value: None},
+    Item{name: b"location", value: None},
+
+    Item{name: b"max-forwards", value: None},
+    Item{name: b"proxy-authenticate", value: None},
+    Item{name: b"proxy-authorization", value: None},
+
+    Item{name: b"range", value: None},
+    Item{name: b"referer", value: None},
+    Item{name: b"refresh", value: None},
+    Item{name: b"retry-after", value: None},
+
+    Item{name: b"server", value: None},
+    Item{name: b"set-cookie", value: None},
+    Item{name: b"strict-transport-security", value: None},
+
+    Item{name: b"transfer-encoding", value: None},
+    Item{name: b"user-agent", value: None},
+    Item{name: b"vary", value: None},
+    Item{name: b"via", value: None},
+    Item {name: b"www-authenticate", value: None},
+];
+
+type HeaderIndexMap = BTreeMap<&'static [u8], usize>;
+type SizedHeaderIndexMap = BTreeMap<usize, HeaderIndexMap>;
+
+type ValueIndexMap = BTreeMap<&'static [u8], usize>;
+type HeaderValueIndexMap = BTreeMap<&'static [u8], ValueIndexMap>;
+type SizedHeaderValueIndexMap = BTreeMap<usize, HeaderValueIndexMap>;
+
+pub struct Seeker {
+    full_headers: SizedHeaderValueIndexMap,
+    no_value_headers: SizedHeaderIndexMap,
+}
+
+impl Seeker {
+    pub fn new() -> Seeker {
+        let mut res = Seeker{
+            full_headers: SizedHeaderValueIndexMap::new(),
+            no_value_headers: SizedHeaderIndexMap::new()};
+        for idx in 1..RAW_TABLE.len() {
+            let name = RAW_TABLE[idx].name;
+            let value = RAW_TABLE[idx].value;
+            match value {
+                Some(value) => {
+                    let r = res.full_headers
+                        .entry(name.len())
+                        .or_insert(HeaderValueIndexMap::new())
+                        .entry(name)
+                        .or_insert(ValueIndexMap::new())
+                        .insert(value, idx);
+                    assert!(r.is_none());
+                },
+                None => {
+                    let r = res.no_value_headers
+                        .entry(name.len())
+                        .or_insert(HeaderIndexMap::new())
+                        .insert(name, idx);
+                    assert!(r.is_none());
+                }
+            };
+        }
+        res
+    }
+
+    pub fn seek(&self, header: &[u8], value: &[u8]) -> Option<usize> {
+        let res = self.seek_in_no_value_headers(header);
+        if res.is_some() {
+            return res;
+        }
+
+        self.seek_in_full_headers(header, value)
+    }
+
+    fn seek_in_no_value_headers(&self, header: &[u8]) -> Option<usize> {
+        let header_idx_map = self.no_value_headers.get(&header.len())?;
+        let idx = header_idx_map.get(header)?;
+        Some(*idx)
+    }
+
+    fn seek_in_full_headers(&self, header: &[u8], value: &[u8]) -> Option<usize> {
+        let header_value_idx_map = self.full_headers.get(&header.len())?;
+        let value_idx_map = header_value_idx_map.get(header)?;
+        let idx = value_idx_map.get(value)?;
+        Some(*idx)
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_static_table_seeker_exhaustive() {
+        let seeker = Seeker::new();
+        let none = b"";
+        
+        for oracle_idx in 1..RAW_TABLE.len() {
+            let header = RAW_TABLE[oracle_idx].name;
+            let value = RAW_TABLE[oracle_idx].value;
+
+            let trial_idx = match value {
+                Some(ref v) => seeker.seek(header, v),
+                None => seeker.seek(header, none),
+            };
+
+            assert_eq!(trial_idx, Some(oracle_idx));
+        }
+    }
+
+    #[test]
+    fn test_static_table_seeker_nonexist_header() {
+        let seeker = Seeker::new();
+        let res = seeker.seek(b"NOT_EXIST", b"WHATEVER");
+        assert!(res.is_none());
+    }
+
+    #[test]
+    fn test_static_table_seeker_nonexist_value () {
+        let seeker = Seeker::new();
+        let res = seeker.seek(b":status", b"NOT_EXIST");
+        assert!(res.is_none());
+    }
+}
