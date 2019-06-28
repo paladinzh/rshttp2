b438863611dff1f2754dc3615ce083dc425960fc
Taoda <tyf00@aliyun.com>
2019-06-28 12:22:45 +0800

    AnySliceable

--- src/sliceable.rs
+++ src/sliceable.rs
@@ -7,3 +7,18 @@ impl<T> Sliceable<T> for Vec<T> {
         self.as_slice()
     }
 }
+
+
+pub struct AnySliceable<T>(Box<dyn Sliceable<T>>);
+
+impl<T> AnySliceable<T> {
+    pub fn new(obj: impl Sliceable<T> + 'static) -> AnySliceable<T> {
+        AnySliceable(Box::new(obj))
+    }
+}
+
+impl<T> Sliceable<T> for AnySliceable<T> {
+    fn as_slice(&self) -> &[T] {
+        self.0.as_slice()
+    }
+}
